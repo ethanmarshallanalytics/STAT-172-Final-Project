21e45b39f35897d0f5951fec16aba4a0d075060e
jschwartzy <jack.schwartz@drake.edu>
2022-11-28 10:33:08 -0600

    Update STAT 172 FINAL PROJECT.R
    adding in the cleaning code

--- STAT 172 FINAL PROJECT.R
+++ STAT 172 FINAL PROJECT.R
@@ -1,3 +1,45 @@
 # STAT 172 Final Project
 
 #doing stuff and things
+
+#reading in data sets:
+
+rm(list=ls())
+
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+skater_stats <-read.csv("Final_project/game_skater_stats.csv")
+player_info <-read.csv("Final_project/player_info.csv")
+
+#adjusting data sets to get desired columns
+skater_stats <- subset(skater_stats, select = -c(evenTimeOnIce, shortHandedTimeOnIce, powerPlayTimeOnIce))
+player_info <- subset(player_info, select = c(player_id, firstName, lastName, nationality, primaryPosition, birthDate, height_cm, weight, shootsCatches))
+
+skater_stats <- na.omit(skater_stats) # this might put some random attributes on column and cause problems for the random forest
+
+
+#merging datasets
+skater_stats <- left_join(skater_stats, player_info, by="player_id")
+
+skater_stats <- na.omit(skater_stats)
+
+#reducing number of rows in data set by only selecting 
+skater_stats <- filter(skater_stats, team_id == 6)
+
+data <- subset(skater_stats, select = -c(game_id, player_id, team_id))
+data$birthDate <- substr(data$birthDate, 1,10)
+
+#fixing the birthDate column to age
+data$birthDate <- as_date(data$birthDate)
+data$age <- (2022 - year(data$birthDate))
+
+#subsetting data to get final clean and organized data set
+data <- subset(data, select = -c(birthDate))
+data <- subset(data, select = -c(powerPlayGoals, shortHandedGoals))
+data$score <- ifelse(data$goals >= 1, "Yes","No")
+data <- subset(data, select = -c(goals))
+data
+nrow(data)
+str(data)
