21f1b3b6348960f41671b9cdc0c8e271617a712c
Nick PIttman <nicholasjohn.pittman@gmail.com>
2022-12-07 09:30:26 -0600

    Remove goals variable after plots are made

--- STAT 172 FINAL PROJECT.R
+++ STAT 172 FINAL PROJECT.R
@@ -61,7 +61,6 @@ data$age <- (2022 - year(data$birthDate))
 #subsetting data to get final clean and organized data set
 data <- subset(data, select = -c(birthDate, plusMinus, powerPlayGoals, shortHandedGoals))
 data$score <- ifelse(data$goals >= 1, "Yes","No")
-data <- subset(data, select = -c(goals))
 data
 nrow(data)
 str(data)
@@ -104,6 +103,7 @@ ggplot(data=data) +
   scale_fill_manual(values=c("#010101", "#FFB81C"), "Goals \nScored")
 
 #Coverting character variables to factors to use for forest
+data <- subset(data, select = -c(goals))
 data$firstName <- as.factor(data$firstName)
 data$lastName <- as.factor(data$lastName)
 data$nationality <- as.factor(data$nationality)
